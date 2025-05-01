fdb45f45a3d48c5b93d2b7012c180da5fcaa5aca
Mustafa-Ki-O <robnsonmark11@gmail.com>
2025-05-01 13:59:59 +0300

    final1.3

--- src/components/DownloadFileButton.jsx
+++ src/components/DownloadFileButton.jsx
@@ -1,9 +1,13 @@
-import { Image,Button } from "@mantine/core"
+import { Image,Button,Text } from "@mantine/core"
 import home from '../assets/css/home.module.css';
 import dw from '../assets/vectors/Download.svg';  
 import { useTranslation } from "react-i18next";
 import cv from '../assets/Files/MoustafaHasan-Cv.docx'
+import button from '../assets/css/footer.module.css';
+import { useEffect, useState } from "react";
 const DownloadFileButton = ({visible4}) => {
+
+    const [visible5,setVisible5] =useState(false);
 
     const {t,i18n} = useTranslation()
     const downloadFile = async () => {
@@ -20,12 +24,25 @@ const DownloadFileButton = ({visible4}) => {
         window.URL.revokeObjectURL(url);
       };
 
+      useEffect(()=>{
+
+        if(visible4){
+            setTimeout(()=>{
+                setVisible5(true)
+            },700)
+        }
+        
+      },[visible4])
+
     return(
 
         <>
-            <Button mt={'10rem'} radius={'md'} variant={'filled'} color={'#08454C'} size="xl" 
-                    className={`${home.fadeButton} ${visible4 ? home.visibleButton : ''}`} onClick={downloadFile} >
+            <Button pos={'relative'} mt={'10rem'} radius={'md'} variant={'filled'} color={'#08454C'} size="xl" 
+                    className={`${home.fadeButton} ${visible4 ? home.visibleButton : ''} `} onClick={downloadFile} >
+                        <Text fz={'1.3rem'} c={'#fff'} className={`${visible5 ? home.textShown :home.textHide}`}>
                         {t('Download CV')}
+                        </Text>
+                        <div className={`${home.circle} ${visible5 ?home.visiblec : ''}`}></div>
                         <Image className={visible4 ? home.download: ''} ml={10} src={dw} w={25}/>
                     </Button>
         </>
