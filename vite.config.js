0e63aa15478b2f802d7e02b2eaa5559e1c67a724
Mustafa-Ki-O <robnsonmark11@gmail.com>
2025-05-03 20:22:56 +0300

    ready for production& deploy-final.1

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true, 
-        pure_funcs: ['console.log'], 
-      },
-    },
-  },
   plugins: [react()],
   base: '/',
   assetsInclude: ['**/*.docx','**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg'],
